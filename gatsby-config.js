2845d25253c3eca39ad3f31864adcd1835d8d2c2
Daniel Kim <dgkim@ucdavis.edu>
2021-09-24 16:06:00 -0700

    added

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,5 +47,20 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: "@chakra-ui/gatsby-plugin",
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: true,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    }
   ],
 }
