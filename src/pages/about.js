5f3a63416dc6bf00c7735caf01abf44994f84395
Daniel Kim <dgkim@ucdavis.edu>
2021-09-26 10:34:27 -0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,6 @@ import React from 'react'
 import {Link} from 'gatsby'
 import {Grid} from 'react-styled-flexboxgrid'
 
-import {Box} from '../components/elements/layout'
 import Layout from './../components/Layout'
 import {Heading1, Lead} from '../components/elements/text'
 
