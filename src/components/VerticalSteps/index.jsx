14f45712cc3a1eac83a2489afe7c6d56fa3175a3
Daniel Kim <dgkim@ucdavis.edu>
2021-09-27 16:31:54 -0700

    add

---
+++ src/components/VerticalSteps/index.jsx
@@ -0,0 +1,130 @@
+import { Box, Stack, Button, Text, HStack, Img, Link} from '@chakra-ui/react'
+import * as React from 'react'
+import { Step } from './Step'
+import { StepContent } from './StepContent'
+import { Steps } from './Steps'
+import { useSteps } from './useSteps'
+
+const VerticalSteps = () => {
+  const { nextStep, prevStep, reset, activeStep } = useSteps({
+    initialStep: 0,
+  })
+  return (
+    <Box
+      mx="auto"
+      maxW="2xl"
+      py="10"
+      px={{
+        base: '6',
+        md: '8',
+      }}
+      minH="400px"
+    >
+      <Steps activeStep={activeStep}>
+        <Step title="Sign up for always-free New Relic One and Get API Keys">
+          <StepContent>
+            <Stack shouldWrapChildren spacing="4">
+              <Text>
+                New Relic One is a powerful full-stack data analysis platform for all your software's metrics, events, and logs.  
+              </Text>
+              <Button
+                mt="8"
+                minW="14rem"
+                colorScheme="pink"
+                size="lg"
+                height="14"
+                bg="#FF0081"
+                px="8"
+                fontSize="md"
+                fontWeight="bold"
+                _hover={{
+                  bg: "#b8005d"
+                }}
+              >
+                Sign up for New Relic One
+              </Button>
+              <Img
+                objectFit="cover"
+                w="full"
+                h="full"
+                htmlWidth="576px"
+                htmlHeight="420px"
+                src="/nr1.png"
+                alt="Bring team together"
+                />
+                <b>Get New Relic Ingest Key, Insert Key, and Account ID </b>
+              <HStack>
+                <Button size="sm" variant="ghost" isDisabled>
+                  Back
+                </Button>
+                <Button size="sm" onClick={nextStep}>
+                  Next
+                </Button>
+              </HStack>
+            </Stack>
+          </StepContent>
+        </Step>
+        <Step title="Deploy Sample Site">
+          <StepContent>
+            <Stack shouldWrapChildren spacing="4">
+              <b>Choose a place to one click deploy</b>
+              <Link href="https://app.netlify.com/start/deploy?repository=https://github.com/lazyplatypus/jank-stack" isExternal>
+              <Img
+                objectFit="cover"
+                w="full"
+                h="full"
+                maxWidth="200px"
+                src="https://www.netlify.com/img/deploy/button.svg"
+                alt="Bring team together"
+                />
+                </Link>
+              <Link href="https://www.gatsbyjs.com/dashboard/deploynow?url=https://github.com/lazyplatypus/jank-stack" isExternal>
+              <Img
+                objectFit="cover"
+                w="full"
+                h="full"
+                maxWidth="200px"
+                src="https://www.gatsbyjs.com/deploynow.svg"
+                alt="Bring team together"
+                />
+                </Link>
+              <HStack>
+                <Button size="sm" onClick={prevStep} variant="ghost">
+                  Back
+                </Button>
+                <Button size="sm" onClick={nextStep}>
+                  Next
+                </Button>
+              </HStack>
+            </Stack>
+          </StepContent>
+        </Step>
+        <Step title="Tweet @newrelic and @gatsbyjs with your dashboard">
+          <StepContent>
+            <Stack shouldWrapChildren spacing="4">
+              <Text>
+                Tag @newrelic and @gatsbyjs with the autoinstrumented dashboard. 
+              </Text>
+              <HStack>
+                <Button size="sm" onClick={prevStep} variant="ghost">
+                  Back
+                </Button>
+                <Button size="sm" onClick={nextStep}>
+                  Finish
+                </Button>
+              </HStack>
+            </Stack>
+          </StepContent>
+        </Step>
+      </Steps>
+      <HStack display={activeStep === 3 ? 'flex' : 'none'} mt="10" spacing="4" shouldWrapChildren>
+        <Text>All steps completed - you&apos;re finished</Text>
+        <Button size="sm" onClick={reset} variant="outline" verticalAlign="baseline">
+          Reset
+        </Button>
+      </HStack>
+    </Box>
+  )
+}
+
+export default VerticalSteps
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import { Navbar } from '../components/Navbar/App'
 import Banner from '../components/Banner'
+import VerticalSteps from '../components/VerticalSteps'
+
 import Feature from '../components/Features'
 
 import {
@@ -27,7 +29,7 @@ const About = () => {
 			<Box
 			maxW={{
 				base: 'xl',
-				md: '7xl',
+				md: '2xl',
 			}}
 			mx="auto"
 			px={{
@@ -65,7 +67,7 @@ const About = () => {
 				</Center>
 				<Box
 				maxW={{
-					lg: 'lg',
+					lg: '2xl',
 				}}
 				>
 				<Heading
@@ -80,95 +82,11 @@ const About = () => {
 				<Text fontSize="lg" mt="6" color={mode('gray.600', 'gray.400')}>
 					Instrument your Gatsby Site with Observability and win a free power bank *(Until Supplies Last). Terms and Conditions apply.
 				</Text>
-				<Button
-					className="group"
-					mt="8"
-					colorScheme="pink"
-					size="lg"
-					px="8"
-					fontWeight="bold"
-					h="14"
-					iconSpacing="3"
-				>
-					Three Steps Below 👇
-				</Button>
+				<VerticalSteps />
 				</Box>
 			</Stack>
 			</Box>
 		</Box>
-		<Box as="section" bg={mode('gray.10', 'gray.800')} py="20">
-		<Box
-		  maxW={{
-			base: 'xl',
-			md: '7xl',
-		  }}
-		  mx="auto"
-		  px={{
-			base: '6',
-			md: '8',
-		  }}
-		>
-		  <Stack
-			spacing={{
-			  base: '4',
-			  lg: '20',
-			}}
-			direction={{
-			  base: 'column',
-			  lg: 'row',
-			}}
-		  >
-			<Center
-			  flex="1"
-			  minH="26rem"
-			  maxW={{
-				lg: 'xl',
-			  }}
-			>
-			  <Img
-				objectFit="cover"
-				w="full"
-				h="full"
-				htmlWidth="576px"
-				htmlHeight="420px"
-				src="/nr1.png"
-				alt="Bring team together"
-			  />
-			</Center>
-			<Box
-			  maxW={{
-				lg: 'lg',
-			  }}
-			>
-			  <Heading
-				size="2xl"
-				mt="10"
-				fontWeight="extrabold"
-				letterSpacing="tight"
-				lineHeight="normal"
-			  >
-				Sign up for a Free New Relic Account and get API Keys
-			  </Heading>
-			  <Text fontSize="lg" mt="6" color={mode('gray.600', 'gray.400')}>
-			  New Relic One is a powerful full-stack data analysis platform for all your software's metrics, events, and logs. 
-			  </Text>
-			  <Button
-				className="group"
-				mt="8"
-				colorScheme="pink"
-				size="lg"
-				px="8"
-				fontWeight="bold"
-				h="14"
-				iconSpacing="3"
-			  >
-				Sign up for New Relic One
-			  </Button>
-			</Box>
-		  </Stack>
-		  <Divider my="20" opacity={1} />
-		</Box>
-	  </Box>
 	</>
 	)
   }
